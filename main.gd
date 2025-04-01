23feb00c6fff801b9a48eb2ffe6a79b295e47b32
OceanOC <141111946+OceanOC@users.noreply.github.com>
2025-04-01 08:34:15 +0530

    Fix gray void when recording and on main menu

--- main.gd
+++ main.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
 var sets = Bvhsets
-
+var recording = true
+var recframes = 0
 var time_accum = 0.0  # Accumulated time
 
-@onready var LH = get_node("SubViewport/XROrigin3D/LHand")
-@onready var RH = get_node("SubViewport/XROrigin3D/RHand")
-@onready var Hed = get_node("SubViewport/XROrigin3D/XRCamera3D")
+@onready var env = get_node("WorldEnvironment/AnimationPlayer")
 @onready var vern = get_node("Label")
+@onready var rectime = get_node("XROrigin3D/XRCamera3D/Recording Time")
+
+@onready var LH = get_node("XROrigin3D/LHand")
+@onready var RH = get_node("XROrigin3D/RHand")
+@onready var Hed = get_node("XROrigin3D/XRCamera3D")
+
 
 var template = """HIERARCHY
   ROOT chest
@@ -55,10 +60,11 @@ func _ready() -> void:
 
 		# Change our main viewport to output to the HMD
 		get_viewport().use_xr = true
+		recording = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
-		vern.set_text("OpenXR failed to start.\nPlease check if your HMD is connected.")
-		
+		vern.set_text("OpenXR failed to start.\nPlease check if your HMD is connected and restart the application.")
+		recording =false
 	
 	
 	get_tree().set_auto_accept_quit(false)
@@ -71,7 +77,10 @@ func _process(delta: float) -> void:
 	
 	
 	if time_accum >= (sets.bvhms / 1000):  # 33.33333 ms
-		temp += do_motionframe()
+		if recording:
+			recframes += 1
+			rectime.text = "%02d Frames" % recframes
+			temp += do_motionframe()
 		time_accum = 0.0  # Reset the accumulated time
 	
 	
@@ -80,24 +89,28 @@ func _process(delta: float) -> void:
 func do_motionframe() -> String:
 	# Shitty way to do this but it works for now
 	var templat: String
-	templat += "0.0 0.0 0.0 0.0 0.0 0.0 "
+	# Chest Bone
+	templat = "0.0 0.0 0.0 0.0 0.0 0.0 "
 	var hd = Hed.get_global_transform().basis.z
 	var hd_pitch = -asin(hd.y)
 	var hd_yaw = atan2(hd.x, -hd.z)
 	
-	templat += "%0.6f" % rad_to_deg(hd_pitch) + " " + "%0.6f" % rad_to_deg(hd_yaw) + " " + "%0.6f" % rad_to_deg(0) + " " + "%0.6f" % Hed.global_position.x + " " + "%0.6f" % Hed.global_position.y + " " + "%0.6f" % Hed.global_position.z + " "
+	# Head Bone
+	templat += "%0.6f" % rad_to_deg(hd_pitch) + " %0.6f" % rad_to_deg(hd_yaw) + " %0.6f" % rad_to_deg(0) + " %0.6f" % Hed.global_position.x + " %0.6f" % Hed.global_position.y + " %0.6f" % Hed.global_position.z + " "
 	
+	# Left Hand Bone
 	var lh = LH.get_global_transform().basis.z
 	var lh_pitch = -asin(lh.y)
 	var lh_yaw = atan2(lh.x, -lh.z)
 	#templat += "%0.6f" % LH.rotation.x + " " + "%0.6f" % LH.rotation.y + " " + "%0.6f" % LH.rotation.z + " " + "%0.6f" % LH.global_position.x + " " + "%0.6f" % LH.global_position.y + " " + "%0.6f" % LH.global_position.z + " "
-	templat += "%0.6f" % rad_to_deg(lh_pitch) + " " + "%0.6f" % rad_to_deg(lh_yaw) + " " + "%0.6f" % rad_to_deg(0) + " " + "%0.6f" % LH.global_position.x + " " + "%0.6f" % LH.global_position.y + " " + "%0.6f" % LH.global_position.z + " "
+	templat += "%0.6f" % rad_to_deg(lh_pitch) + " %0.6f" % rad_to_deg(lh_yaw) + " %0.6f" % rad_to_deg(0) + " %0.6f" % LH.global_position.x + " %0.6f" % LH.global_position.y + " %0.6f" % LH.global_position.z + " "
 	
+	# Right Hand Bone
 	var rh = RH.get_global_transform().basis.z
 	var rh_pitch = -asin(rh.y)
 	var rh_yaw = atan2(rh.x, -rh.z)
 	#templat += "%0.6f" % RH.rotation.x + " " + "%0.6f" % RH.rotation.y + " " + "%0.6f" % RH.rotation.z + " " + "%0.6f" % RH.global_position.x + " " + "%0.6f" % RH.global_position.y + " " + "%0.6f" % RH.global_position.z + "\n"
-	templat += "%0.6f" % rad_to_deg(rh_pitch) + " " + "%0.6f" % rad_to_deg(rh_yaw) + " " + "%0.6f" % rad_to_deg(0) + " " + "%0.6f" % RH.global_position.x + " " + "%0.6f" % RH.global_position.y + " " + "%0.6f" % RH.global_position.z + "\n"
+	templat += "%0.6f" % rad_to_deg(rh_pitch) + " %0.6f" % rad_to_deg(rh_yaw) + " %0.6f" % rad_to_deg(0) + " %0.6f" % RH.global_position.x + " %0.6f" % RH.global_position.y + " %0.6f" % RH.global_position.z + "\n"
 	return templat
 
 func _notification(what):
@@ -107,8 +120,13 @@ func _notification(what):
 
 
 func _on_L_hand_button_pressed(name: String) -> void:
-	if (name == "ax_button"):
+	if (name == "ax_button" || Input.is_action_pressed("endrecord")):
 		var file = FileAccess.open(sets.bvhfile, FileAccess.WRITE)
 		file.store_string(temp)
+		
+		
 		get_tree().quit()
+		#env.play("desat")
+		#recording =false
+		
 	pass # Replace with function body.
